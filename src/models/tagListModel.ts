94d2d7e0f0286c92c6241caea99f159e6c394149
lyhhh <17610603221@163.com>
2020-09-15 21:46:20 +0800

    使用全局 tagList

--- src/models/tagListModel.ts
+++ src/models/tagListModel.ts
@@ -1,22 +1,8 @@
 import createId from '@/lib/createId';
 
 const localStorageKeyName = 'tagList';
-type Tag = {
-	id: string;
-	name: string;
-}
-type TagListModel = {
-	data: Tag[];
-	fetch: () => Tag[];
-	create: (name: string) => 'success' | 'duplicated';
-	update: (id: string, name: string) => 'success' | 'not found' | 'duplicated';
-	remove: (id: string) => boolean;
-	save: () => void;
-}
-
 const tagListModel: TagListModel = {
 	data: [],
-	
 	fetch() {
 		this.data = JSON.parse(window.localStorage.getItem(localStorageKeyName) || '[]');
 		return this.data;
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -17,24 +17,14 @@
 	import Tags from '@/components/Money/Tags.vue';
 	import {Component, Watch} from 'vue-property-decorator';
 	import recordListModel from '@/models/recordListModel';
-	import tagListModel from '@/models/tagListModel';
 	
 	const recordList = recordListModel.fetch();
-	const tagList = tagListModel.fetch();
-	
-	type RecordItem = {
-		tags: string[];
-		notes: string;
-		type: string;
-		amount: number; // 数据类型
-		createdAt?: Date; // 类/构造函数
-	}
 	
 	@Component({
 		components: {Tags, FormItem, Types, NumberPad}
 	})
 	export default class Money extends Vue {
-		tags = tagList;
+		tags = window.tagList;
 		recordList: RecordItem[] = recordList;
 		record: RecordItem = {tags: [], notes: '', type: '-', amount: 0};
 		
