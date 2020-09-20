907938e08fb9e259f9582d92ab8ea6b853bd59e5
lyhhh <17610603221@163.com>
2020-09-20 16:28:59 +0800

    删除 Types.vue

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -2,7 +2,7 @@
 	<Layout class-prefix="layout">
 		<NumberPad :value.sync="record.amount" @submit="saveRecord"/>
 		<Tabs :data-source="recordTypeList"
-					:value.sync="record.type"/>
+		      :value.sync="record.type"/>
 		<div class="formWrapper">
 			<FormItem @update:value="OnUpdateNotes" field-name="备注" placeholder="点击输入备注"/>
 		</div>
@@ -13,7 +13,6 @@
 <script lang="ts">
 	import Vue from 'vue';
 	import NumberPad from '@/components/Money/NumberPad.vue';
-	import Types from '@/components/Money/Types.vue';
 	import FormItem from '@/components/Money/FormItem.vue';
 	import Tags from '@/components/Money/Tags.vue';
 	import {Component} from 'vue-property-decorator';
@@ -21,7 +20,7 @@
 	import Tabs from '@/components/Tabs.vue';
 	
 	@Component({
-		components: {Tabs, Tags, FormItem, Types, NumberPad},
+		components: {Tabs, Tags, FormItem, NumberPad},
 	})
 	export default class Money extends Vue {
 		record: RecordItem = {tags: [], notes: '', type: '-', amount: 0};
