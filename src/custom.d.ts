779f3796be94b56080d92f8810db7c13077080d3
lyhhh <17610603221@163.com>
2020-09-21 17:11:01 +0800

    完成列表的 CSS

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -1,18 +1,15 @@
+type RootState = {
+	recordList: RecordItem[];
+	tagList: Tag[];
+	currentTag?: Tag;
+}
+
 type RecordItem = {
 	tags: string[];
 	notes: string;
 	type: string;
 	amount: number; // 数据类型
-	createdAt?: Date; // 类/构造函数
-}
-
-type TagListModel = {
-	data: Tag[];
-	fetch: () => Tag[];
-	create: (name: string) => 'success' | 'duplicated';
-	update: (id: string, name: string) => 'success' | 'not found' | 'duplicated';
-	remove: (id: string) => boolean;
-	save: () => void;
+	createdAt?: string; // 类/构造函数
 }
 
 type Tag = {
--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -2,25 +2,34 @@
 	<Layout>
 		<Tabs class-prefix="type" :data-source="recordTypeList" :value.sync="recordType"/>
 		<Tabs class-prefix="interval" :data-source="intervalList" :value.sync="interval"/>
-		<div>
-			type: {{recordType}}
-			<br/>
-			interval: {{interval}}
-		</div>
+		<ol>
+			<li v-for="(group,index) in result"
+			    :key="index">
+				<h3 class="title">{{group.title}}</h3>
+				<ol v-for="item in group.items"
+				    :key="item.id"
+				    class="record">
+					<span>{{tagString(item.tags)}}</span>
+					<span class="notes">{{item.notes}}</span>
+					<span>￥{{item.amount}}</span>
+				</ol>
+			</li>
+			<!--	下面是用来测试的		-->
+			<li v-for="(group,index) in result"
+			    :key="index">
+				<h3 class="title">{{group.title}}</h3>
+				<ol v-for="item in group.items"
+				    :key="item.id"
+				    class="record">
+					<span>{{tagString(item.tags)}}</span>
+					<span class="notes">{{item.notes}}</span>
+					<span>￥{{item.amount}}</span>
+				</ol>
+			</li>
+			<!--	上面是用来测试的		-->
+		</ol>
 	</Layout>
 </template>
-
-<style lang="scss" scoped>
-	::v-deep .type-tabs-item {
-		background: white;
-		&.selected {
-			background: #c4c4c4;
-			&::after {
-				display: none;
-			}
-		}
-	}
-</style>
 
 <script lang="ts">
 	import Vue from 'vue';
@@ -33,9 +42,76 @@
 		components: {Tabs},
 	})
 	export default class Statistics extends Vue {
+		tagString(tags: Tag[]) {
+			return tags.length === 0 ? '无' : tags.join(',');
+		}
+		
+		get recordList() {
+			return (this.$store.state as RootState).recordList;
+		}
+		
+		get result() {
+			const {recordList} = this;
+			type HashTableValue = { title: string; items: RecordItem[] };
+			
+			const hashTable: { [key: string]: HashTableValue } = {};
+			for (let i = 0; i < recordList.length; i++) {
+				const [date] = recordList[i].createdAt!.split('T');
+				hashTable[date] = hashTable[date] || {title: date, items: []};
+				//TODO
+				hashTable[date].items.push(recordList[i]);
+			}
+			console.log(hashTable);
+			return hashTable;
+		}
+		
+		beforeCreate() {
+			this.$store.commit('fetchRecords');
+		}
+		
 		recordType = '-';
 		interval = 'day';
 		intervalList = intervalList;
 		recordTypeList = recordTypeList;
 	}
 </script>
+
+<style lang="scss" scoped>
+	::v-deep {
+		.type-tabs-item {
+			background: white;
+			&.selected {
+				background: #c4c4c4;
+				&::after {
+					display: none;
+				}
+			}
+		}
+		li.interval-tabs-item {
+			height: 48px;
+		}
+	}
+	%item {
+		padding: 8px 16px;
+		line-height: 24px;
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+	}
+	.title {
+		@extend %item;
+	}
+	.record {
+		@extend %item;
+		background: white;
+	}
+	.notes {
+		margin-right: auto;
+		margin-left: 16px;
+		color: #999;
+		white-space: nowrap;
+		text-overflow: ellipsis;
+		overflow: hidden;
+	}
+
+</style>
